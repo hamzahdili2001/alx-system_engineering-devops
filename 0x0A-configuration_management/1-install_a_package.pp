50e6e6d838ce7435886fd9a78e0c3357bde5a523
hamzahdili2001 <hdilihamza955@gmail.com>
2024-01-19 17:33:30 +0100

    [update] task-1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,6 @@
-# install_flask.pp
-package { 'python3-pip':
-  ensure => 'installed',
-}
-package { 'Flask':
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip',
-  require  => Package['python3-pip'],
+  provider => 'pip3'
 }
