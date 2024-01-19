4dfd453b6e5e7b77732d1bd1e15f4cf474f30faa
hamzahdili2001 <hdilihamza955@gmail.com>
2024-01-19 17:34:51 +0100

    [update] task-1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Ensure that the python3-pip package is installed
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+# Install Flask using pip3 with the specified version
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/local/bin:/usr/bin',
+  creates => '/usr/local/lib/python3.8/dist-packages/flask',
 }
