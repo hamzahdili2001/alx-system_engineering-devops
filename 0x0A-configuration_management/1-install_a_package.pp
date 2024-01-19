47db33a4e04c01ed044bf973404a6621676a3e30
hamzahdili2001 <hdilihamza955@gmail.com>
2024-01-19 17:25:52 +0100

    [update] task-1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,9 @@
-# install_flask
+# Using Puppet, install flask from pip3.
+# Requirements:
+#     Install flask
+#     Version must be 2.1.0
 
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'flask':
+ensure   => '2.1.0',
+provider => 'pip3',
 }
