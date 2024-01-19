aed85c2039980f078c3c3bbb48c701d41c459d38
hamzahdili2001 <hdilihamza955@gmail.com>
2024-01-19 17:46:17 +0100

    [update] task-1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,18 @@
-# install flask from pip3.
-package {'flask':
+# install flask
+
+# Install python3-pip package
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+# Install flask using pip3
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+# Install Werkzeug using pip3
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
