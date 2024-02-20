04e035be42e3058d16ed49e4bdfe71ded6ac4976
hamzahdili2001 <hdilihamza955@gmail.com>
2024-02-20 01:18:38 +0100

    [add] task-1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+ extend your Python script to export data in the CSV format.
+"""
+
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users"
+    user = requests.get(url + "/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "/{}/todos".format(sys.argv[1])).json()
+    username = user.get("username")
+
+    with open("{}.csv".format(user_id), "w", newline="") as csfile:
+        writer = csv.writer(csfile, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            writer.writerow(
+                [
+                    sys.argv[1],
+                    username,
+                    task.get("completed"),
+                    task.get("title"),
+                ]
+            )
