6c8adae84a7446516a629eacc41ff5836441d916
hamzahdili2001 <hdilihamza955@gmail.com>
2024-03-05 16:55:43 +0100

    [try-fix] task-2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,39 +4,33 @@ and returns a list containing the titles of all hot articles"""
 import requests
 
 
-def recurse(subreddit, hot_list=None, after=None):
-    """Recursively fetches hot articles from a subreddit."""
-
-    if hot_list is None:
-        hot_list = []
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-
-    params = (
-        {"limit": 100, "after": after} if after else {"limit": 100}
+def recurse(subreddit, hot_list=[]):
+    """Return hot articles for a given subreddit"""
+    return recurse_util(
+        subreddit=subreddit, hot_list=hot_list, after=None
     )
 
-    response = requests.get(url, headers=headers, params=params)
 
+def recurse_util(subreddit, hot_list=[], after=None):
+    """An util for the recurse function"""
+    if not hot_list:
+        hot_list = []
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    params = {"limit": 100, "after": after}
+    response = requests.get(
+        url,
+        headers={"User-agent": "MyRedditBot/1.0 (fily)"},
+        params=params,
+    )
     if response.status_code == 200:
         data = response.json()
-        if (
-            "data" in data
-            and "children" in data["data"]
-            and len(data["data"]["children"]) > 0
-        ):
-            for child in data["data"]["children"]:
-                hot_list.append(child["data"]["title"])
-            if data["data"]["after"]:
-                return recurse(
-                    subreddit, hot_list, data["data"]["after"]
-                )
-            else:
-                return hot_list
+        posts = data.get("data").get("children")
+        for post in posts:
+            hot_list.append(post.get("data").get("title"))
+        after = data.get("data").get("after")
+        if after:
+            return recurse_util(subreddit, hot_list, after)
         else:
-            return None
-    elif response.status_code == 404:
-        raise Expception
+            return hot_list
     else:
-        return Expception
+        return None
