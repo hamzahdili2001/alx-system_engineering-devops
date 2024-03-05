5fa3e1a267a35bf32f18e1fa82f52448f642245e
hamzahdili2001 <hdilihamza955@gmail.com>
2024-03-05 14:27:08 +0100

    [fix] 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,23 +4,39 @@ and returns a list containing the titles of all hot articles"""
 import requests
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
-    """Returns a list of titles of all hot posts on a given subreddit"""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+def recurse(subreddit, hot_list=None, after=None):
+    """Recursively fetches hot articles from a subreddit."""
+
+    if hot_list is None:
+        hot_list = []
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    params = {"after": after, "count": count, "limit": 100}
-    response = requests.get(
-        url, headers=headers, params=params, allow_redirects=False
+
+    params = (
+        {"limit": 100, "after": after} if after else {"limit": 100}
     )
-    if response.status_code == 404:
-        return None
 
-    results = response.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        hot_list.append(c.get("data").get("title"))
+    response = requests.get(url, headers=headers, params=params)
 
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
+    if response.status_code == 200:
+        data = response.json()
+        if (
+            "data" in data
+            and "children" in data["data"]
+            and len(data["data"]["children"]) > 0
+        ):
+            for child in data["data"]["children"]:
+                hot_list.append(child["data"]["title"])
+            if data["data"]["after"]:
+                return recurse(
+                    subreddit, hot_list, data["data"]["after"]
+                )
+            else:
+                return hot_list
+        else:
+            return None
+    elif response.status_code == 404:
+        raise Expception
+    else:
+        return Expception
