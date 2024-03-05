85966e47909e10115b9db6c8dcbe59b7ee3c626b
hamzahdili2001 <hdilihamza955@gmail.com>
2024-03-05 15:11:10 +0100

    [add] task-0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,9 @@ def number_of_subscribers(subreddit):
 
     URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-    headers = {"User-Agent": "Reddit-Agent"}
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
     response = requests.get(
         URL, headers=headers, allow_redirects=False
     )
