8973d333aa5cb4777dd020fed828f6935c69c71d
hamzahdili2001 <hdilihamza955@gmail.com>
2024-03-05 14:03:20 +0100

    [add] 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,10 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "linux:0-subs:v1.0.0 (by /u/hamzed)"}
     response = requests.get(
         url, headers=headers, allow_redirects=False
     )
-    if response.status_code != 200:
+    if response.status_code == 404:
         return 0
     return response.json().get("data").get("subscribers")
